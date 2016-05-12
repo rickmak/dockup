feb2ad21ea58c09f5940a9c79132adba54bd7228
Rick Mak <rick.mak@gmail.com>
2016-05-12 15:02:27 +0800

    Use STANDARD_IA storage class for s3

--- backup.sh
+++ backup.sh
@@ -9,7 +9,7 @@ readonly tarball=$BACKUP_NAME$BACKUP_SUFFIX.tar.gz
 tar czf $tarball $BACKUP_TAR_OPTION $PATHS_TO_BACKUP
 
 # Upload the backup to S3 with timestamp
-aws s3 --region $AWS_DEFAULT_REGION cp $tarball s3://$S3_BUCKET_NAME/$tarball
+aws s3 --region $AWS_DEFAULT_REGION cp --storage-class=STANDARD_IA $tarball s3://$S3_BUCKET_NAME/$tarball
 
 # Clean up
 rm $tarball
