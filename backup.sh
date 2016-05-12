df93d58c85daf76abd3a0231603421f2528f2ef3
Rick Mak <rick.mak@gmail.com>
2016-05-12 15:03:11 +0800

    Add mysql backup

--- backup.sh
+++ backup.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 export PATH=$PATH:/usr/bin:/usr/local/bin:/bin
+: ${MYSQL_DUMP:='dump.sql'}
 # Get timestamp
 : ${SUFFIX:='%Y-%m-%d-%H-%M-%S'}
 : ${BACKUP_SUFFIX:=.$(date +"$SUFFIX")}
 readonly tarball=$BACKUP_NAME$BACKUP_SUFFIX.tar.gz
 
+if [[ -n "$MYSQL_DB" ]]; then
+    mysqldump -u $MYSQL_USER -p$MYSQL_PASS --single-transaction -B $MYSQL_DB > $MYSQL_DUMP
+fi
+
 # Create a gzip compressed tarball with the volume(s)
-tar czf $tarball $BACKUP_TAR_OPTION $PATHS_TO_BACKUP
+tar czf $tarball $BACKUP_TAR_OPTION $PATHS_TO_BACKUP $MYSQL_DUMP
 
 # Upload the backup to S3 with timestamp
 aws s3 --region $AWS_DEFAULT_REGION cp --storage-class=STANDARD_IA $tarball s3://$S3_BUCKET_NAME/$tarball
 
 # Clean up
 rm $tarball
+rm $MYSQL_DUMP
--- restore.sh
+++ restore.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+: ${MYSQL_DUMP:='dump.sql'}
 
 # Find last backup file
 : ${LAST_BACKUP:=$(aws s3 ls s3://$S3_BUCKET_NAME | awk -F " " '{print $4}' | grep ^$BACKUP_NAME | sort -r | head -n1)}
@@ -8,3 +9,8 @@ aws s3 cp s3://$S3_BUCKET_NAME/$LAST_BACKUP $LAST_BACKUP
 
 # Extract backup
 tar xzf $LAST_BACKUP $RESTORE_TAR_OPTION
+
+# Restoring mysql dump
+if [[ -n "$MYSQL_DB" ]]; then
+    mysql -u $MYSQL_USER -p$MYSQL_PASS < $MYSQL_DUMP
+fi
