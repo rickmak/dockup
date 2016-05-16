01cc572e75c59a06ab56ca9777d850b145e8d50a
Rick Mak <rick.mak@gmail.com>
2016-05-16 11:36:58 +0800

    Make MYSQL dump to tmp
    Try to get rig of permission problem during cron

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ else
 fi
 
 if [ -n "$CRON_TIME" ]; then
-  echo "${CRON_TIME} /backup.sh >> /dockup.log 2>&1" > /crontab.conf
+  echo "${CRON_TIME} cd /tmp && /backup.sh >> /dockup.log 2>&1" > /crontab.conf
   crontab  /crontab.conf
   echo "=> Running dockup backups as a cronjob for ${CRON_TIME}"
   env >> /etc/environment
