f63e7df36909459ad82ca63e797d79ec0af8e269
Rick Mak <rick.mak@gmail.com>
2016-05-16 11:22:03 +0800

    Run the cron -f directly

--- run.sh
+++ run.sh
@@ -10,5 +10,6 @@ if [ -n "$CRON_TIME" ]; then
   echo "${CRON_TIME} /backup.sh >> /dockup.log 2>&1" > /crontab.conf
   crontab  /crontab.conf
   echo "=> Running dockup backups as a cronjob for ${CRON_TIME}"
-  exec cron -f
+  env >> /etc/environment
+  cron -f
 fi
